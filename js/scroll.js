29fa388d3e04687dc247b3ebbc0e1765b3eb68fd
yeowool <qyoong3579@gmail.com>
2022-08-07 23:00:01 +0900

    콘솔 주석처리

--- js/scroll.js
+++ js/scroll.js
@@ -1,9 +1,9 @@
 $(document).ready(function () {
     var tmp = parseInt($("#move-nav").css('top'));
-    console.log(tmp)
-    console.log(parseInt("-3"))
-    console.log($("#move-nav").css('top'))
-    console.log(Number($("#move-nav").css('top')))
+    // console.log(tmp)
+    // console.log(parseInt("-3"))
+    // console.log($("#move-nav").css('top'))
+    // console.log(Number($("#move-nav").css('top')))
 
     $(window).scroll(function () {
         var scrollTop = $(window).scrollTop();
