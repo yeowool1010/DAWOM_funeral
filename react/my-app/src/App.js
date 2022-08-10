5709dfffb1dd6d67d25adb64f800b21378a44cfd
yeowool <qyoong3579@gmail.com>
2022-08-11 02:22:13 +0900

    react-start

---
+++ react/my-app/src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+         다움상조 페이지 리뉴얼 준비중입니다.
+        </p>
+        <a
+          className="App-link"
+          href="https://yeowool1010.github.io/DAWOM_funeral/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          다움상조 기존 페이지로 이동
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
+
+2022.08.11_기존 프로젝트 리액트로 다시 시작하기
