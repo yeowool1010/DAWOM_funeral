f1a315301dec236f9f78ede7df1243091a2acc85
yeowool <qyoong3579@gmail.com>
2022-08-18 23:23:48 +0900

    디렉토리 구성

--- react/my-app/src/App.js
+++ react/my-app/src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         다움상조 페이지 리뉴얼 준비중입니다.
-        </p>
-        <a
-          className="App-link"
-          href="https://yeowool1010.github.io/DAWOM_funeral/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          다움상조 기존 페이지로 이동
-        </a>
-      </header>
+  
     </div>
   );
 }
