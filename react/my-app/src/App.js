197bbd8a9b2a2598a4240e4634cebc45ed22cb3b
yeowool <qyoong3579@gmail.com>
2022-08-19 01:29:08 +0900

    Main-nav-bar_start

--- react/my-app/src/App.js
+++ react/my-app/src/App.js
@@ -1,11 +1,19 @@
+import React from 'react';
 import './App.css';
+// import {BrowserRouter, Routes, Route} from "react-router-dom";
 
+import MainNavBar from './Main-nav-bar'
 
 function App() {
   return (
+  <div>
     <div className="App">
-  
+      <main>
+        <MainNavBar />
+        
+      </main>
     </div>
+  </div>
   );
 }
 
